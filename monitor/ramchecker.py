a79a4cd30d31ee20d1bcd01aafc22d021eeee38e
Tech-Overlord <am900820@gmail.com>
2018-08-14 19:46:10 +0500

    call added for filegenerator module's win_folder function if threshold is exceeded.

--- monitor/ramchecker.py
+++ monitor/ramchecker.py
@@ -1,14 +1,16 @@
 #! /usr/bin/env python3
 
 import monitor.varslist as v
+import monitor.filegenerator as fg
 
 
 def ram_check():
     if v.ram_percentage >= v.threshold:
         print('RAM usage has exceeded the specified threshold, which is: ' + str(v.threshold) + '%')
         print('Current RAM usage is: ' + str(v.ram_percentage) + '%')
+        fg.win_folder()
     else:
         print("RAM usage is under threshold limit.")
 
 
-# ram_check()
+ram_check()
