dc9833750f501fedcb78c2297090a8d2b7dc7129
Ali Muhammad <am900820@gmail.com>
2018-08-16 14:06:35 +0500

    added docstring for ram_check() function.

--- monitor/ramchecker.py
+++ monitor/ramchecker.py
@@ -5,6 +5,12 @@ import monitor.filegenerator as fg
 
 
 def ram_check():
+    """
+    IF/ELSE statements to verify whether the current RAM utilization exceeds the specified threshold.
+    IF RAM utilization exceeds than the threshold, then print the utilization recorded, along with threshold and then
+    call the function win_folder() from monitor.filegenerator module.
+    Else a message is printed suggesting that the RAM usage is under the specified threshold.
+    """
     if v.ram_percentage >= v.threshold:
         print('RAM usage has exceeded the specified threshold, which is: ' + str(v.threshold) + '%')
         print('Current RAM usage is: ' + str(v.ram_percentage) + '%')
