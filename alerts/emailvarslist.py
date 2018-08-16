75f002a3b53c5085d42e87701fd59d71aacd3568
Ali Muhammad <am900820@gmail.com>
2018-08-16 10:09:20 +0500

    added a variable list file for email usage to store confidential information.

---
+++ alerts/emailvarslist.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+
+# Variables for SMTP connection details such as SMTP URL and SMTP PORT
+smtp_url = 'smtp.gmail.com'
+smtp_port = 465
+
+# Variables for Sender's Email & Password
+sender_email = str('sender@gmail.com')
+sender_password = str('password')
+
+# Variable for Receiver's Email.
+receiver_email = str('receiver@gmail.com')
+
+# Gmail's link to enable use by less secure apps.
+# https://myaccount.google.com/lesssecureapps?pli=1
