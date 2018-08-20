02beccb4116beb9b6838e11011cdd03f97805d12
Ali Muhammad <am900820@gmail.com>
2018-08-20 11:15:43 +0500

    changes based on the update to emailvarslist, introduction of MIME functions

--- alerts/sendemail.py
+++ alerts/sendemail.py
@@ -3,18 +3,20 @@
 import smtplib
 import alerts.emailvarslist as ev
 
+
 # SMTP object that represents connection to a SMTP server. This is connecting using SSL.
-smtpObj = smtplib.SMTP_SSL(ev.smtp_url, ev.smtp_port)
+server = smtplib.SMTP_SSL(ev.smtp_url, ev.smtp_port)
 
 # Confirming connection with SMTP Server.
-smtpObj.ehlo()
+server.ehlo()
 
 
 # STMP login details. Email & Password.
-smtpObj.login(ev.sender_email, ev.sender_password)
+server.login(ev.sender_email, ev.sender_password)
+
 
 # Sending email. First is the FROM address, second is the TO address followed by Subject and message in new line.
-smtpObj.sendmail(ev.sender_email, ev.receiver_email, 'Subject: Hi again!.\nThis is a test email')
+server.sendmail(ev.sender_email, ev.receiver_email, ev.text)
 
 # Exiting SMTP connection.
-smtpObj.quit()
+server.quit()
