66d1a3b03cf44dd79ffd425d79f0ecbcd7985fad
Tech-Overlord <am900820@gmail.com>
2018-09-21 15:13:00 +0500

    pre-req python installation script for Windows platform

---
+++ pre-reqs/install_pre-reqs.ps1
@@ -0,0 +1,15 @@
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted
+
+## Downloading Chocolatey, installing and upgrading it
+$script = New-Object Net.WebClient
+$script.DownloadString("https://chocolatey.org/install.ps1")
+iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+choco upgrade chocolatey
+
+## Download and Install Python3
+choco install -y python3
+refreshenv
+python -V
+
+## Upgrade pip
+python -m pip install --upgrade pip
