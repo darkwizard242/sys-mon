1df5f341f2b287438158b5e0910da186ee014d36
Tech-Overlord <am900820@gmail.com>
2018-09-22 00:27:42 +0500

    python install set to ProgramData\Python370

--- pre-reqs/install_pre-reqs.ps1
+++ pre-reqs/install_pre-reqs.ps1
@@ -1,20 +1,12 @@
 $env:ChocolateyInstall="$env:ProgramData\chocoportable"
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-
-
-#$InstallDir="$choco_dir"
-#$env:ChocolateyInstall="$InstallDir"
-#Set-ExecutionPolicy Bypass -Scope CurrentUser
-#iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-#Set-ExecutionPolicy Bypass -Force -Scope CurrentUser
-
 ## Downloading Chocolatey, installing and upgrading it
-#iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
 choco upgrade chocolatey
 
 ## Download and Install Python3
-choco install -y python3
+$env:PythonInstall="$env:ProgramData\Python370"
+choco install -y python3 --install-directory "$env:PythonInstall"
 refreshenv
 python -V
 
