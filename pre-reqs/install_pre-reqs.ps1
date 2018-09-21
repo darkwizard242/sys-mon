d5a30979663d8d3fd8b414dd5f4277415d1604b4
Tech-Overlord <am900820@gmail.com>
2018-09-21 23:52:53 +0500

    commented out execution policy overriding code.

--- pre-reqs/install_pre-reqs.ps1
+++ pre-reqs/install_pre-reqs.ps1
@@ -1,11 +1,11 @@
-$InstallDir= "$choco_dir"
+$InstallDir="$choco_dir"
 $env:ChocolateyInstall="$InstallDir"
-Set-ExecutionPolicy Bypass -Scope CurrentUser
+#Set-ExecutionPolicy Bypass -Scope CurrentUser
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 ## Set-ExecutionPolicy Bypass -Force -Scope CurrentUser
 
 ## Downloading Chocolatey, installing and upgrading it
-iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+#iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
 choco upgrade chocolatey
 
 ## Download and Install Python3
